75f3e9a5d9c3ed995ae7dd76c745965da93431c8
JJLYHTGP <zhangch99@mail2.sysu.edu.cn>
2018-12-02 21:12:20 +0800

    [TS]类、函数、泛型

--- Web/TypeScript/demo/docstudy/main.ts
+++ Web/TypeScript/demo/docstudy/main.ts
@@ -23,6 +23,9 @@ enum Color6 {Red=1, Green=1, Blue=1}
 console.log(Color, Color2, Color3, Color4, Color5, Color6);
 
 // 接口
+/*
+
+// 额外的属性检查
 interface SquareConfig {
   color: string;
   width?: number;
@@ -40,14 +43,17 @@ function createSquare(config: SquareConfig): { color: string; area: number } {
   return newSquare;
 }
 
+// 只读属性
 interface Point {
   readonly x: number;
   readonly y: number;
 }
 
+
 let p1: Point = { x: 10, y: 10 };
 p1.x = 10; // error
 
+// ReadonlyArray<T>
 let a: number[] = [1];
 let ro: ReadonlyArray<number> = a;
 let ro2: ReadonlyArray<number> = a;
@@ -56,3 +62,61 @@ ro.push(1); // error: property 'push' doesnot exist
 ro.length = 100 // error: a constant or read-only property
 a = ro; // type ReadonlyArray<number> is not assignable to number[], property 'pop' is missing on type ReadonlyArray<number> 
 ro2 = ro; // OK
+
+*/
+
+// 类类型
+interface ClockConstructor {
+  new (hour: number, minute: number): any;
+}
+
+class Clock implements ClockConstructor {
+  currentTime: Date;
+  constructor(h: number, m: number) { }
+}
+/* Class 'Clock' incorrectly implements interface 'ClockConstructor'.
+  Type 'Clock' provides no match for the signature 
+  'new (hour: number, minute: number): any'. 
+*/
+
+
+abstract class Animal {
+  abstract makeSound(): void;
+  move(): void {
+      console.log('roaming the earch...');
+  }
+}
+
+abstract class Department {
+
+  constructor(public name: string) {
+  }
+
+  printName(): void {
+      console.log('Department name: ' + this.name);
+  }
+
+  abstract printMeeting(): void; // 必须在派生类中实现
+}
+
+class AccountingDepartment extends Department {
+
+  constructor() {
+      super('Accounting and Auditing'); // 在派生类的构造函数中必须调用 super()
+  }
+
+  printMeeting(): void {
+      console.log('The Accounting Department meets each Monday at 10am.');
+  }
+
+  generateReports(): void {
+      console.log('Generating accounting reports...');
+  }
+}
+
+let department: Department; // 允许创建一个对抽象类型的引用
+department = new Department(); // 错误: 不能创建一个抽象类的实例
+department = new AccountingDepartment(); // 允许对一个抽象子类进行实例化和赋值
+department.printName();
+department.printMeeting();
+department.generateReports(); // 错误: 方法在声明的抽象类中不存在
